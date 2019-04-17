a7b8e6811c93d3ed07aa5b05062bc40c4c265e71
liaoyan-work <363918226@qq.com>
2019-04-17 13:57:01 +0800

    首页商品显示

--- controllers/goods.go
+++ controllers/goods.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"dailyfresh/models"
+)
 
 type GoodsController struct {
 	beego.Controller
@@ -16,7 +20,28 @@ func GetUser(self *beego.Controller) string  {
 	return userName.(string)
 }
 
+/*展示首页*/
 func (self *GoodsController) ShowIndex() {
-	GetUser(&self.Controller)
+	//GetUser(&self.Controller)
+	o := orm.NewOrm()
+
+	// 获取类型数据
+	var goodsType []models.GoodsType
+	o.QueryTable(&models.GoodsType{}).All(&goodsType)
+	self.Data["goodsTypes"] = goodsType
+
+	// 获取轮播图数据
+	var indexGoods []models.IndexGoodsBanner
+	o.QueryTable(&models.IndexGoodsBanner{}).OrderBy("Index").All(&indexGoods)
+	self.Data["indexGoodsBanner"] = indexGoods
+
+	// 获取促销商品数据
+	var promotionGoods []models.IndexPromotionBanner
+	o.QueryTable(&models.IndexPromotionBanner{}).OrderBy("Index").All(&promotionGoods)
+	self.Data["promotionGoods"] = promotionGoods
+
+
+	// 获取首页商品数据
+
 	self.TplName = "index.html"
 }
